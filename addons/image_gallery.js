072107ae07a75936a37e2614378c3589c3dbdbba
Elliot Smith <elliot.smith@qedas.com>
2016-08-07 14:31:48 +0100

    TN - Image Viewer
    Adjustments to fade in / fade out

--- addons/image_gallery.js
+++ addons/image_gallery.js
@@ -2,7 +2,7 @@ $('img[alt*="g%"]').each(function(k,v){
 	var alt = $(this).attr("alt");
 	x = alt.search("g%");
 	v = alt.charAt(x+2)
-	$(this).wrap("<div class='image x"+v+"' />");
+	$(this).wrap("<div class='image_gallery x"+v+"' />");
 	$(this).before("<div class='overlay'><span class='icon-zoom-in'></span></div>");
 	if(k === 0) {
 		$(this).parent().before("<div class='fix' />");
@@ -11,7 +11,7 @@ $('img[alt*="g%"]').each(function(k,v){
 
 $(function(){
     $.fn.reverse = [].reverse;
-    var target = '.image', // the class of the elements to group
+    var target = '.image_gallery', // the class of the elements to group
         invert = ':not(' + target + ')', // we create the invert to find the breakpoints
         wrap = '<div class="img_gallery">', // what to wrap with
         breakpoints = $('p > *'+invert); // find the breakpoints in our container
@@ -22,7 +22,7 @@ $(function(){
 });
 
 function setHeight() {
-	$(".image").each(function() {
+	$(".image_gallery").each(function() {
 		w = $(this).width();
 		sh = w * 9/14;
 		$(this).height(sh);
@@ -32,8 +32,27 @@ function setHeight() {
 }
 
 $(".overlay").click(function(){
-	var i = $(this).next().attr("src");
-	console.log(i);
+	var url = $(this).next().attr("src");
+	var ghost = $(".site-wrapper");
+	var iv = $("<div id='image_viewer'/>");
+	var x = $("<div id='close_image_viewer' class='icon-close'></div>");
+	var ph = $("<div class='image_placeholder'/>");
+	var img = $('<img>').attr('src', url);
+	
+	if($('#image_viewer').length == 0) {
+		iv.prependTo(ghost);
+		x.appendTo(iv);
+		ph.appendTo(iv);
+		img.appendTo(ph);
+		
+		iv.hide();
+		iv.fadeIn('slow');
+		x.on("click",function(){
+			iv.fadeOut('slow', function(){
+				$(this).remove();
+			});
+		});
+	}
 });
 
 $(window).load(function(){
